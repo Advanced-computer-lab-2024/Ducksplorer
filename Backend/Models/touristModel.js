30c9cc5f002debf8f0b9702cee2ecd2a4aaf5842
JaydaaRoushdy <jaydaa.roushdy@gmail.com>
2024-09-26 15:40:24 +0300

    Commit 1

---
+++ Backend/Models/touristModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const touristSchema = new  Schema({
+// touristID:{
+//     type : String,
+//     default: uuidv4
+// },
+    email:{
+        type : String,
+        required : true
+    },
+    userName:{
+        type : String,
+        required : true
+    },
+    password :{
+        type : String ,
+        required :true
+    },
+    mobileNumber:{
+        type : Number,
+        required : true
+    },
+    nationality:{
+        type : String,
+        required : true,
+        default : Egyptian
+    },
+    DOB : {
+        type : Date,
+        required : true,
+    },
+    employmentStatus:{
+        required : true ,
+        type : String
+    },
+},{timestamps: true})
+
+
+const Tourist = mongoose.model("Tourist",touristSchema);
+
+
+module.exports = Tourist;
+
+
---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config(); //makes us read the env file
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express(); //el kol fel kol
+const PORT = process.env.PORT || 8000; //tells us to get port from env file or law ma3refsh yegebha it's 3000
+
+console.log(process.env.PORT );
+app.use(express.json());
+
+
+const connectToMongoDB = async () => {
+    try {
+        await mongoose.connect(process.env.Ducksplorer_URI);
+        console.log("Mongo Connected Successfully");
+    }
+    catch(error){
+        console.log("Error Connecting to MongoDB", error.message)
+    }
+}
+
+app.listen(PORT, () => {
+    connectToMongoDB(); 
+    console.log(`Server Running on Port ${PORT}`)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
