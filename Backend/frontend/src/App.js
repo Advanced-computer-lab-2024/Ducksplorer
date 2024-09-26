1eaacc37294497ea898b94379290fc948c7ea4f0
JaydaaRoushdy <jaydaa.roushdy@gmail.com>
2024-09-26 16:45:57 +0300

    test me

---
+++ Backend/frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import FormSection from './Components/FormSection.js';
+import TextSection from './Components/TextSection.js';
+
+function App() {
+  return (
+    <>
+    <FormSection/>
+    <TextSection/>
+    </>
+
+  );
+}
+
+export default App;
