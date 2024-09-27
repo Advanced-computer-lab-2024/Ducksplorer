73af7ba682f8d37a94549e1154d2b8fa38f53ca9
mowaell <mwael0304@gmail.com>
2024-09-27 17:06:59 +0300

    fixed the Signup problem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
-import FormSection from './Components/FormSection.js';
-import TextSection from './Components/TextSection.js';
-import {useState} from 'react';
 import React from 'react';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Signup from './Routes/Signup.js';
 
-   /*const [type, updateType] = useState("Tourist");
+   
 
-
-
-  const newType = (thisType)=>
-  {
-    updateType(thisType);
-  }
-
- */
+ 
 function App() {
-  
-
 
   return (
-    <>
-    <FormSection type={"Tourist"}/>
-    <TextSection/>
-    </>
-
+    <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/signUp" element=  {<Signup/>} />  
+      </Routes>
+    </BrowserRouter>
+    </React.StrictMode>
   );
 }
 
 
 
 export default App;
-export {newType} from "./App.js";
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
-import App from './App';
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
-import DropDown from './Components/DropDown.js';
-import TextSection from './Components/TextSection.js';
-import FormSection from './Components/FormSection.js';
-import Header from './Components//TopNav/header.js';
+import App from './App'; 
+import { TypeContextProvider } from './context/TypeContext';
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path = "/signUp" element=  {<Header/>} />  
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-  
+    <TypeContextProvider>
+      <App/>
+    </TypeContextProvider>
+
 );
+
 
 
