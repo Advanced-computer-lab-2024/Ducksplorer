6d668f49bd9fee4eed8c4deb92157dce72c62b2a
YFouda <yfoudadev@gmail.com>
2024-11-02 21:12:29 +0200

    multer added

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const mongoose = require("mongoose");
 const app = express(); //el kol fel kol
 const PORT = process.env.PORT || 8000; //tells us to get port from env file or law ma3refsh yegebha it's 3000
 const cors = require("cors");
+const touristRoutes = require("./Backend/Routes/touristRoutes.js");
+const sellerRoutes = require("./Backend/Routes/sellerRoutes.js");
+const adminProductRoutes = require("./Backend/Routes/adminRoutes.js");
+const cors = require("cors");
 const multer = require("multer");
 const touristRoutes = require("./Backend/Routes/touristRoutes.js");
 const sellerRoutes = require("./Backend/Routes/sellerRoutes.js");
@@ -15,12 +19,24 @@ const AdminActivityRoutes = require("./Backend/Routes/Admin/AdminActivityRoutes.
 const preferenceTagsRoutes = require("./Backend/Routes/Admin/PreferenceTagsRoutes.js");
 const activityRoutes = require("./Backend/Routes/activityRoutes.js");
 const categoryRoutes = require("./Backend/Routes/categoryRoutes.js");
+const multer = require("multer");
 
 app.use(cors());
+
+app.use("/uploads", express.static("uploads"));
 
 console.log(process.env.PORT);
 app.use(express.json());
 
+const museumRoutes = require("./Backend/Routes/museumHistoricalPlaceRoutes/museumRoutes.js");
+const historicalPlaceRoutes = require("./Backend/Routes/museumHistoricalPlaceRoutes/historicalPlaceRoutes.js");
+const historicalPlaceTagRoutes = require("./Backend/Routes/museumHistoricalPlaceRoutes/historicalPlaceTagRoutes.js");
+const museumTagRoutes = require("./Backend/Routes/museumHistoricalPlaceRoutes/museumTagRoutes.js");
+const itineraryRoutes = require("./Backend/Routes/itineraryRoutes.js");
+const tourGuideAccountRoutes = require("./Backend/Routes/TourGuideAccountRoutes.js");
+const advertiserAccountRoutes = require("./Backend/Routes/AdvertiserAccountRoutes.js");
+const sellerAccountRoutes = require("./Backend/Routes/SellerAccountRoutes.js");
+const complaintRoutes = require("./Backend/Routes/complaintRoutes.js");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "uploads/"); // Folder where images will be stored
