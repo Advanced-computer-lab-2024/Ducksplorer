0fc73a2f5f4e52d694c47dae95359c3fed5b8874
mowaell <mwael0304@gmail.com>
2024-11-23 21:11:09 +0200

    notifications added

---
+++ Backend/Models/Notifications.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const notificationSchema = new Schema({
+    user: {
+        type: String,
+        ref: 'Tourist',
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: false
+    },
+    seen: {
+        type: Boolean,
+        required: false
+    },
+});
+
+const Notification = mongoose.model('Notification', notificationSchema);
+module.exports = Notification;
