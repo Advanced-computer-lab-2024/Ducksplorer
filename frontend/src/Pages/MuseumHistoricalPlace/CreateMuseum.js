aac5b7c73641f11f61d2733531759a653cbfce1f
YFouda <yfoudadev@gmail.com>
2024-12-04 11:55:08 +0200

    Create museum and create Historical place half fixed

--- frontend/src/Pages/MuseumHistoricalPlace/CreateMuseum.js
+++ frontend/src/Pages/MuseumHistoricalPlace/CreateMuseum.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import MuseumForm from '../../Components/MuseumHistoricalPlaceComponent/MuseumForm';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import MuseumForm from "../../Components/MuseumHistoricalPlaceComponent/MuseumForm";
+import { Link } from "react-router-dom";
 
 const CreateMuseum = () => {
-
-    return (
-        <div>
-            <Link to="/governorDashboard"> Back </Link>
-            <MuseumForm />
-        </div>
-    )
-}
+  return (
+    <div>
+      <Link to="/governorDashboard"> Back </Link>
+      <MuseumForm />
+    </div>
+  );
+};
 
 export default CreateMuseum;
