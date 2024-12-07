f17f6793a9c73166539801413ec49b5873c5afa2
YFouda <yfoudadev@gmail.com>
2024-12-07 13:29:17 +0200

    fixed Advertiser Tables

--- frontend/src/Pages/Itinerary/RUDItinerary.js
+++ frontend/src/Pages/Itinerary/RUDItinerary.js
@@ -7,7 +7,7 @@ import CurrencyConvertor from "../../Components/CurrencyConvertor";
 import WarningIcon from "@mui/icons-material/Warning";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { useNavigate } from "react-router-dom";
-
+import TourGuideNavbar from "../../Components/NavBars/TourGuideNavBar";
 import {
   IconButton,
   Button,
