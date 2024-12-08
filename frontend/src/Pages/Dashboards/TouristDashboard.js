abb829606eab9c56f30cc301875916f1ed0d87e6
omartamerr10 <omar.kamel@student.guc.edu.eg>
2024-12-08 16:06:30 +0200

    Omar Commit adjusted video for tourist dashboard

--- frontend/src/Pages/Dashboards/TouristDashboard.js
+++ frontend/src/Pages/Dashboards/TouristDashboard.js
@@ -48,7 +48,7 @@ const TouristDashboard = () => {
         style={{
           position: "fixed",
           top: 0,
-          left: "5%",
+          left: "0%",
           width: "100%",
           height: "100%",
           objectFit: "cover",
